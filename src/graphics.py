152aeca1186ce555c97cd1ab24dd36698a249a15
Harsh <unagctystudent2@gmail.com>
2020-01-20 17:46:40 -0500

    transparency and hotbar

--- src/graphics.py
+++ src/graphics.py
@@ -71,21 +71,18 @@ def draw_surface(plist, draw_points, draw_lines, color, transparent):
         draw_line(plist[1], plist[2], draw_points, color)
         draw_line(plist[2], plist[3], draw_points, color)
         draw_line(plist[3], plist[0], draw_points, color)
-    if transparent:
-        color_obj = pygame.Color(color[0], color[1], color[2], 64)
-        if top_forward:
-            pygame.gfxdraw.filled_polygon(top_screen, [(int(coords_set_1[0][0]), int(coords_set_1[0][1])), (int(coords_set_2[0][0]), int(coords_set_2[0][1])), (int(coords_set_3[0][0]), int(coords_set_3[0][1])), (int(coords_set_4[0][0]), int(coords_set_4[0][1]))], color_obj)
-        if mid_forward:
-            pygame.gfxdraw.filled_polygon(mid_screen, [(int(coords_set_1[1][0]), int(coords_set_1[1][1])), (int(coords_set_2[1][0]), int(coords_set_2[1][1])), (int(coords_set_3[1][0]), int(coords_set_3[1][1])), (int(coords_set_4[1][0]), int(coords_set_4[1][1]))], color_obj)
-        if bottom_forward:
-            pygame.gfxdraw.filled_polygon(bottom_screen, [(int(coords_set_1[2][0]), int(coords_set_1[2][1])), (int(coords_set_2[2][0]), int(coords_set_2[2][1])), (int(coords_set_3[2][0]), int(coords_set_3[2][1])), (int(coords_set_4[2][0]), int(coords_set_4[2][1]))], color_obj)
+    if transparent == 2:
+        color_obj = pygame.Color(int(color[0]), int(color[1]), int(color[2]), 63)
+    elif transparent == 1:
+        color_obj = pygame.Color(int(color[0]), int(color[1]), int(color[2]), 127)
     else:
-        if top_forward:
-            pygame.draw.polygon(top_screen, color, [(int(coords_set_1[0][0]), int(coords_set_1[0][1])), (int(coords_set_2[0][0]), int(coords_set_2[0][1])), (int(coords_set_3[0][0]), int(coords_set_3[0][1])), (int(coords_set_4[0][0]), int(coords_set_4[0][1]))])
-        if mid_forward:
-            pygame.draw.polygon(mid_screen, color, [(int(coords_set_1[1][0]), int(coords_set_1[1][1])), (int(coords_set_2[1][0]), int(coords_set_2[1][1])), (int(coords_set_3[1][0]), int(coords_set_3[1][1])), (int(coords_set_4[1][0]), int(coords_set_4[1][1]))])
-        if bottom_forward:
-            pygame.draw.polygon(bottom_screen, color, [(int(coords_set_1[2][0]), int(coords_set_1[2][1])), (int(coords_set_2[2][0]), int(coords_set_2[2][1])), (int(coords_set_3[2][0]), int(coords_set_3[2][1])), (int(coords_set_4[2][0]), int(coords_set_4[2][1]))])
+        color_obj = pygame.Color(int(color[0]), int(color[1]), int(color[2]), 255)
+    if top_forward:
+        pygame.gfxdraw.filled_polygon(top_screen, [(int(coords_set_1[0][0]), int(coords_set_1[0][1])), (int(coords_set_2[0][0]), int(coords_set_2[0][1])), (int(coords_set_3[0][0]), int(coords_set_3[0][1])), (int(coords_set_4[0][0]), int(coords_set_4[0][1]))], color_obj)
+    if mid_forward:
+        pygame.gfxdraw.filled_polygon(mid_screen, [(int(coords_set_1[1][0]), int(coords_set_1[1][1])), (int(coords_set_2[1][0]), int(coords_set_2[1][1])), (int(coords_set_3[1][0]), int(coords_set_3[1][1])), (int(coords_set_4[1][0]), int(coords_set_4[1][1]))], color_obj)
+    if bottom_forward:
+        pygame.gfxdraw.filled_polygon(bottom_screen, [(int(coords_set_1[2][0]), int(coords_set_1[2][1])), (int(coords_set_2[2][0]), int(coords_set_2[2][1])), (int(coords_set_3[2][0]), int(coords_set_3[2][1])), (int(coords_set_4[2][0]), int(coords_set_4[2][1]))], color_obj)
 
 def add_cube(corner):
     global surfaces
@@ -159,8 +156,9 @@ def playing(pos):
     local_cube = [int(2 * math.floor(pos[0] / 2)), int(2 * math.floor(pos[1] / 2)), int(2 * math.floor(pos[2] / 2)), 0, 127, 0]
     return local_cube not in cubes
 
-side_size = (600, 450)
-mid_size = (900, 900)
+side_size = (450, 450)
+mid_size = (720, 720)
+items_size = (mid_size[0], side_size[1] * 2 - mid_size[1])
 
 coords = [1, -5, 1]
 theta = math.pi / 2
@@ -176,16 +174,32 @@ total_screen = pygame.display.set_mode((side_size[0] + mid_size[0], 2 * side_siz
 top_screen = pygame.Surface(side_size)
 mid_screen = pygame.Surface(mid_size)
 bottom_screen = pygame.Surface(side_size)
+items_screen = pygame.Surface(items_size)
 
 font = pygame.font.SysFont("Courier", 50)
+little_font = pygame.font.SysFont("Courier", 20, bold = True)
+top_text = little_font.render("top", True, (127, 127, 127))
+bottom_text = little_font.render("bottom", True, (127, 127, 127))
+front_text = little_font.render("forward", True, (127, 127, 127))
 won_text = font.render("you won!", True, (255, 255, 255))
 lost_text = font.render("you lost!", True, (255, 255, 255))
 build_text = font.render("Build", True, (255, 255, 255))
 play_text = font.render("Play", True, (255, 255, 255))
+
 won_text_rect = won_text.get_rect(center = ((side_size[0] + mid_size[0]) / 2, side_size[1]))
 lost_text_rect = lost_text.get_rect(center = ((side_size[0] + mid_size[0]) / 2, side_size[1]))
 build_text_rect = build_text.get_rect(center = ((side_size[0] + mid_size[0]) / 2, side_size[1] * 2 / 3))
 play_text_rect = play_text.get_rect(center = ((side_size[0] + mid_size[0]) / 2, side_size[1] * 4 / 3))
+
+pickaxe = pygame.transform.scale(pygame.image.load("img/pickaxe.png"), (items_size[1], items_size[1]))
+normal_cube = pygame.transform.scale(pygame.image.load("img/normalcube.png"), (items_size[1], items_size[1]))
+green_cube = pygame.transform.scale(pygame.image.load("img/greencube.png"), (items_size[1], items_size[1]))
+red_cube = pygame.transform.scale(pygame.image.load("img/redcube.png"), (items_size[1], items_size[1]))
+
+pickaxe_rect = pickaxe.get_rect(topleft = (0, 0))
+normal_cube_rect = normal_cube.get_rect(topleft = (items_size[1], 0))
+green_cube_rect = green_cube.get_rect(topleft = (2 * items_size[1], 0))
+red_cube_rect = red_cube.get_rect(topleft = (3 * items_size[1], 0))
 
 f = open("cubes.txt", "r")
 cube_strings = f.readlines()
@@ -200,10 +214,12 @@ for i in cubes:
     add_cube(i)
 
 main_menu = True
+build_loop = False
 game_loop = False
 won = False
 
 timestamp = 0
+buildstamp = 0
 
 while True:
     while main_menu:
@@ -211,7 +227,6 @@ while True:
         total_screen.blit(build_text, build_text_rect)
         total_screen.blit(play_text, play_text_rect)
         pos = pygame.mouse.get_pos()
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -219,6 +234,10 @@ while True:
                 if play_text_rect.collidepoint(pos):
                     main_menu = False
                     game_loop = True
+                elif build_text_rect.collidepoint(pos):
+                    main_menu = False
+                    build_loop = True
+        pygame.display.flip()
     while game_loop:
         won = False
         timestamp = 0
@@ -231,12 +250,13 @@ while True:
         top_screen.fill([0, 0, 0])
         mid_screen.fill([0, 0, 0])
         bottom_screen.fill([0, 0, 0])
+        items_screen.fill([0, 0, 0])
         surfaces.sort(key = lambda surface: get_dist_squared(surface, coords), reverse = True)
         for i in surfaces:
             if i[4] == [-1, -1, -1]:
-                draw_surface(i, False, False, return_color(get_dist_squared(i, coords), get_dist_squared(i, [0, -12, 0])), False)
+                draw_surface(i, False, False, return_color(get_dist_squared(i, coords), get_dist_squared(i, [0, -12, 0])), 0)
             else:
-                draw_surface(i, False, False, i[4], True)
+                draw_surface(i, False, False, i[4], 2)
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_z]:
             if free([coords[0] + 10 * trans_speed * math.cos(theta), coords[1] + 10 * trans_speed * math.sin(theta), coords[2]]):
@@ -264,12 +284,72 @@ while True:
             theta += rot_speed
         if keystate[pygame.K_RIGHT]:
             theta -= rot_speed
+        top_screen.blit(top_text, (10, 10))
+        bottom_screen.blit(bottom_text, (10, 10))
+        mid_screen.blit(front_text, (10, 10))
         total_screen.blit(top_screen, (0, 0))
         total_screen.blit(bottom_screen, (0, side_size[1]))
         total_screen.blit(mid_screen, (side_size[0], 0))
+        total_screen.blit(items_screen, (side_size[0], mid_size[1]))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        pygame.display.flip()
+    while build_loop:
+        if buildstamp % 100 == 0:
+            del surfaces[:]
+            for i in cubes:
+                add_cube(i)
+        buildstamp += 1
+        total_screen.fill([0, 0, 0])
+        top_screen.fill([0, 0, 0])
+        bottom_screen.fill([0, 0, 0])
+        mid_screen.fill([0, 0, 0])
+        items_screen.fill([0, 0, 0])
+        surfaces.sort(key = lambda surface: get_dist_squared(surface, coords), reverse = True)
+        for i in surfaces:
+            if i[4] == [-1, -1, -1]:
+                draw_surface(i, False, False, return_color(get_dist_squared(i, coords), get_dist_squared(i, [0, -12, 0])), 1)
+            else:
+                draw_surface(i, False, False, i[4], 2)
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_z]:
+            coords[0] += trans_speed * math.cos(theta)
+            coords[1] += trans_speed * math.sin(theta)
+        if keystate[pygame.K_x]:
+            coords[0] -= trans_speed * math.cos(theta)
+            coords[1] -= trans_speed * math.sin(theta)
+        if keystate[pygame.K_a]:
+            coords[0] -= trans_speed * math.sin(theta)
+            coords[1] += trans_speed * math.cos(theta)
+        if keystate[pygame.K_d]:
+            coords[0] += trans_speed * math.sin(theta)
+            coords[1] -= trans_speed * math.cos(theta)
+        if keystate[pygame.K_w]:
+            coords[2] += trans_speed
+        if keystate[pygame.K_s]:
+            coords[2] -= trans_speed
+        if keystate[pygame.K_LEFT]:
+            theta += rot_speed
+        if keystate[pygame.K_RIGHT]:
+            theta -= rot_speed
+        top_screen.blit(top_text, (10, 10))
+        bottom_screen.blit(bottom_text, (10, 10))
+        mid_screen.blit(front_text, (10, 10))
+        items_screen.blit(pickaxe, pickaxe_rect)
+        items_screen.blit(normal_cube, normal_cube_rect)
+        items_screen.blit(green_cube, green_cube_rect)
+        items_screen.blit(red_cube, red_cube_rect)
+        total_screen.blit(top_screen, (0, 0))
+        total_screen.blit(bottom_screen, (0, side_size[1]))
+        total_screen.blit(mid_screen, (side_size[0], 0))
+        total_screen.blit(items_screen, (side_size[0], mid_size[1]))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                actual = (pos[0] - side_size[0], pos[1] - mid_size[1])
         pygame.display.flip()
     if timestamp == 0:
         timestamp = pygame.time.get_ticks()
